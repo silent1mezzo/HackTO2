ce7d99811c8282e284ac1a251be0c177c2176be9
Adam McKerlie <adammckerlie@gmail.com>
2012-04-14 10:40:43 -0400

    Initial app

--- hackto/urls.py
+++ hackto/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -14,4 +14,12 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'$', include('around.urls'))
 )
+
+
+
+# ... the rest of your URLconf goes here ...
+
+urlpatterns += staticfiles_urlpatterns()
