654b942e04873ab30c623644c7509b44a429b778
Terence Lo <tlo@gadventures.com>
2012-04-14 09:07:34 -0400

    initial check-in for modified yellow api.

---
+++ hackto/libraries/yellowbetter.py
@@ -0,0 +1,33 @@
+from yellowapi import *
+import json
+import random
+
+class YellowBetterAPI(YellowAPI):
+    def __init__(self, api_key, test_mode=True, format='JSON', handlers=[]):
+        super(YellowBetterAPI, self).__init__(api_key, test_mode, format, handlers)
+
+    def find_restaurant(self, what, where, uid, page=None, page_len=None,
+            sflag=None, lang=None, maxDistance=float(1.0)):
+        what = "restaurant " + what
+        resultsDict = json.loads(self.find_business(what,where,uid,page,page_len, sflag, lang))
+        listings = resultsDict.get('listings')
+        if not listings:
+            return []
+        filteredResults = []
+        for listing in listings:
+            if not listing.get('distance'):
+                continue
+            if float(listing.get('distance')) < float(maxDistance):
+                self.clean_listing(listing)
+                filteredResults.append(listing)
+
+        return filteredResults
+    
+    # given yp listing result, remove unneeded keys
+    def clean_listing(self,listing):
+        keys = ['isParent', 'content', 'parentId']
+        for key in keys:
+            if listing.has_key(key):
+                del listing[key]
+        return listing
+
