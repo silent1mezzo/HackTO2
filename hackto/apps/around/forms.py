48a5e47f7d678b43e06411f4225689e50a98fb37
Terence Lo <tlo@gadventures.com>
2012-04-14 12:55:47 -0400

    single input for address or postal code to be passed as 'where' param.

--- hackto/apps/around/forms.py
+++ hackto/apps/around/forms.py
@@ -2,5 +2,4 @@ from django import forms
 from django.contrib.localflavor.ca.forms import CAPostalCodeField
 
 class SearchForm(forms.Form):
-    address = forms.CharField(widget=forms.TextInput(attrs={'class':'input-xxlarge', 'placeholder':'Enter your address...'}))
-    postal_code = CAPostalCodeField(widget=forms.TextInput(attrs={'class':'input-small', 'placeholder':'Postal Code...'}))
+    address = forms.CharField(widget=forms.TextInput(attrs={'class':'input-xxlarge', 'placeholder':'Enter your address/postal code...'}))
